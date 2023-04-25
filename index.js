88ff0c9a13d7d67d8719c3d221235445004762b6
liisa <liisa-m@outlook.com>
2023-04-25 19:18:50 +0300

    3.2 phonebook backend step2

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  }
+]
+
+app.get('/info', (req, res) => {
+    const ppl = persons.length
+    const todayTime = (new Date()).toString()
+    res.send(`Phonebook has info for ${ppl} people<br>
+    ${todayTime}`)
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    response.json(person)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
