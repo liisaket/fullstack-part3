992321e4b33971c7724c52107a935ab98b4d79f0
liisa <liisa-m@outlook.com>
2023-09-17 15:08:21 +0300

    3.5 puhelinluettelo step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -50,6 +52,25 @@ app.get('/info', (req, res) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    const person = {
+      id: Math.random(),
+      name: body.name,
+      number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
   })
   
   const PORT = 3001
