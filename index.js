8676503e9fc273b1dd5014ce09657c4e293a64df
liisa <liisa-m@outlook.com>
2023-10-05 19:56:20 +0300

    virheiden käsittely

--- index.js
+++ index.js
@@ -54,16 +54,22 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
+/// Virheidenkäsittelyn keskittäminen middlewareen
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (!person) {
-    return response.status(400).json({
-      error: 'content missing'
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
   }
-  response.json(person)
-})
+)
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
