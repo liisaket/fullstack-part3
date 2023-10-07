e94ebcd33e49f2dda6a2f3e5fa0dcf004aa7ae81
liisa <liisa-m@outlook.com>
2023-10-07 21:11:45 +0300

    3.18* step6

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const errorHandler = (error, request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms :body'))
 
