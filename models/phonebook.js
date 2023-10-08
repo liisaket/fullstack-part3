d34b77615ebb3d7af329cf3c323bd7f52cbffd95
liisa <liisa-m@outlook.com>
2023-10-08 22:50:40 +0300

    3.20* step8

--- models/phonebook.js
+++ models/phonebook.js
@@ -22,8 +22,14 @@ const personSchema = new mongoose.Schema({
     },
     number: {
       type: String,
-      minlength: 3,
-      required: true
+      minlength: 8,
+      required: true,
+      validate: {
+        validator: function(v) {
+        return /^\d{2}-\d{6,}|\d{3}-\d{5,}$/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
     }
 })
   
